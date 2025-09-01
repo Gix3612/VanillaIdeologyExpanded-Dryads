f795f12afbfea793a7445f37e4eefa6998c8713b
Gix3612 <108153336+Gix3612@users.noreply.github.com>
2025-09-01 18:56:41 -0400

    Reimplement changes and source folder rebase

---
+++ 1.6/Source/VanillaIdeologyExpanded Dryads/VanillaIdeologyExpanded Dryads/Comps/CompNotifyDestruction.cs
@@ -0,0 +1,44 @@
+﻿
+using Verse;
+using VEF.AnimalBehaviours;
+
+namespace VanillaIdeologyExpanded_Dryads
+{
+    class CompNotifyDestruction : ThingComp
+    {
+
+
+        public CompProperties_NotifyDestruction Props
+        {
+            get
+            {
+                return (CompProperties_NotifyDestruction)this.props;
+            }
+        }
+
+      
+
+        public override void PostDeSpawn(Map map, DestroyMode mode = DestroyMode.Vanish)
+        {
+            NotifyDestructionToComps(map);
+        }
+
+        public override void PostDestroy(DestroyMode mode, Map previousMap)
+        {
+            NotifyDestructionToComps(previousMap);
+        }
+
+        public void NotifyDestructionToComps(Map map)
+        {
+            foreach (Pawn p in map.mapPawns.SpawnedColonyAnimals)
+            {
+                CompBuildPeriodically comp = p.TryGetComp<CompBuildPeriodically>();
+                if (comp != null)
+                {
+                    comp.NotifyBuildingDestroyed(this.parent);
+                }
+            }
+
+        }
+    }
+}
